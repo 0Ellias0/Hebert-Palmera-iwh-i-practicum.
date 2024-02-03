92e47b0590e3b59ccd22a13776283c4576bedad3
0Ellias0 <hpalmerabuelvas@gmail.com>
2024-02-02 20:36:29 -0500

    app.post fixed, now working properly

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
+
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const OBJECT_TYPE_ID = process.env.OBJECT_TYPE_ID;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -42,13 +48,15 @@ app.post('/update-cobj', async(req, res) =>{
     };
 
     //API endpoint 
-    const apiUrl = 'https://api.hubspot.com/crm/v3/objects/custom-object-type-id';
+    const apiUrl = `https://api.hubspot.com/crm/v3/objects/${OBJECT_TYPE_ID}`;
 
     //API access token 
     const headers = {
-        Authorization: 'Bearer ${process.env.PRIVATE_APP_ACCESS}',
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
+
+    // console.log('Resquest headers:', headers);
 
     try {
         // POST request to create a new record in HubSpot 
